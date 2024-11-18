82ac47a023720ee455a6e1c69e15b224999a31a7
omniaosman <UGS.140547@ci.suez.edu.eg>
2024-11-18 12:21:50 +0200

    fix: remove username from register_user

--- accounts/logic.py
+++ accounts/logic.py
@@ -8,15 +8,12 @@ def register_user(data, request):
     email = data['email']
     password = data['password']
     
-    username = email
-    
     # Register user
     User.objects.create_user(
         first_name=first_name,
         last_name=last_name,
         email=email,
         password=password,
-        username=username
     )
     
     return {
