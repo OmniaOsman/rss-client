0f62dd2ddc7a49c05141194b7e928251ade9b359
A.Gamal <50333822+ahmgam@users.noreply.github.com>
2024-12-29 21:22:24 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,8 @@
 python manage.py makemigrations
 # Apply database migrations (uncomment if needed)
 python manage.py migrate
-
-# Collect static files (uncomment if needed)
-#python manage.py collectstatic --noinput
-
+# load data from data seed file to database
+python3 manage.py loaddata 0001_category.json
 # Run the original CMD or any other command you'd like
 python manage.py runserver 0.0.0.0:8000
 exec "$@"
-
-# load data from data seed file to database
-python3 manage.py loaddata 0001_category.json
